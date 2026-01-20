6ff6bc97ea8ac88b8364ff7a028ab844aae12804
hsgamer <huynhqtienvtag@gmail.com>
2026-01-20 22:26:32 +0700

    export two formats

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   exports: true,
+  format: {
+    esm: {
+      target: ['es2015'],
+    },
+    cjs: {
+      target: ['node20'],
+    },
+  },
   // ...config options
 })
